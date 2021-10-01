7f41c27d3b308286cec26012e181e2487d3fa16c
Hemant Singh <hemantsingh@Hemants-Air.lan>
2021-10-01 14:28:25 +0530

    lazy load theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mydemo';
+  themeElm = this.document.getElementById('app-theme');
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  setTheme(event: any): any {
+    console.log('PPA', event.target.value);
+
+    this.renderer.setAttribute(
+      this.themeElm,
+      'href',
+      `/${event.target.value}-theme.css`
+    );
+  }
 }
